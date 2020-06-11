c9bdd2d38cea2f1560c8dff5fe6079c14d8cecce
Laura <laurabeatriserafim@gmail.com>
2020-06-11 08:02:36 -0300

    refactor: use auth layout in signin and signup pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,47 +1,44 @@
 import React from 'react';
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 
+import AuthLayout from '../../layouts/Auth';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
-
-import { Container, Content, Background } from './styles';
+import signInBackground from '../../assets/sign-in-background.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Content>
-        <img src={logo} aria-label="GoBarber" alt="GoBarber" />
+    <AuthLayout backgroundImage={signInBackground}>
+      <img src={logo} aria-label="GoBarber" alt="GoBarber" />
 
-        <form>
-          <h1>Login to the platform</h1>
+      <form>
+        <h1>Login to the platform</h1>
 
-          <Input
-            name="email"
-            type="email"
-            autoCapitalize="none"
-            placeholder="E-mail"
-            icon={FiMail}
-          />
-          <Input
-            name="password"
-            type="password"
-            placeholder="Password"
-            icon={FiLock}
-          />
+        <Input
+          name="email"
+          type="email"
+          autoCapitalize="none"
+          placeholder="E-mail"
+          icon={FiMail}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          icon={FiLock}
+        />
 
-          <Button type="button">Confirm</Button>
+        <Button type="button">Confirm</Button>
 
-          <a href="#">Forgot my password</a>
-        </form>
+        <a href="#">Forgot my password</a>
+      </form>
 
-        <a href="#">
-          <FiLogIn />
-          Create an account
-        </a>
-      </Content>
-      <Background />
-    </Container>
+      <a href="#">
+        <FiLogIn />
+        Create an account
+      </a>
+    </AuthLayout>
   );
 };
 
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,52 +1,49 @@
 import React from 'react';
 import { FiLogIn, FiLock, FiMail, FiUser } from 'react-icons/fi';
 
+import AuthLayout from '../../layouts/Auth';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
-
-import { Container, Content, Background } from './styles';
+import signUpBackground from '../../assets/sign-up-background.png';
 
 const SignUp: React.FC = () => {
   return (
-    <Container>
-      <Background />
-      <Content>
-        <img src={logo} aria-label="GoBarber" alt="GoBarber" />
+    <AuthLayout backgroundPosition="right" backgroundImage={signUpBackground}>
+      <img src={logo} aria-label="GoBarber" alt="GoBarber" />
 
-        <form>
-          <h1>Create an account</h1>
+      <form>
+        <h1>Create an account</h1>
 
-          <Input
-            name="name"
-            type="text"
-            autoCapitalize="none"
-            placeholder="Full name"
-            icon={FiUser}
-          />
-          <Input
-            name="email"
-            type="email"
-            autoCapitalize="none"
-            placeholder="E-mail"
-            icon={FiMail}
-          />
-          <Input
-            name="password"
-            type="password"
-            placeholder="Password"
-            icon={FiLock}
-          />
+        <Input
+          name="name"
+          type="text"
+          autoCapitalize="none"
+          placeholder="Full name"
+          icon={FiUser}
+        />
+        <Input
+          name="email"
+          type="email"
+          autoCapitalize="none"
+          placeholder="E-mail"
+          icon={FiMail}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          icon={FiLock}
+        />
 
-          <Button type="button">Confirm</Button>
-        </form>
+        <Button type="button">Confirm</Button>
+      </form>
 
-        <a href="#">
-          <FiLogIn />
-          Already have an account? Log in
-        </a>
-      </Content>
-    </Container>
+      <a href="#">
+        <FiLogIn />
+        Already have an account? Log in
+      </a>
+    </AuthLayout>
   );
 };
 
