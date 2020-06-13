677ff6a89de4f9404df8dc2461c3d66b0b62eb61
Laura <laurabeatriserafim@gmail.com>
2020-06-13 11:40:11 -0300

    fix: remove unused imports from ripple button

--- src/styles/components/RippleButton.tsx
+++ src/styles/components/RippleButton.tsx
@@ -1,5 +1,4 @@
-import styled, { keyframes, css } from 'styled-components';
-import { rgba, lighten } from 'polished';
+import styled, { keyframes } from 'styled-components';
 
 interface RippleButtonProps {
   className?: string;
