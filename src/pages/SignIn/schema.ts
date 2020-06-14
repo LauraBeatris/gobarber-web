dafd265a96ff48de2f92b2933e82ecd8c95e95c0
Laura <laurabeatriserafim@gmail.com>
2020-06-14 15:50:32 -0300

    fix: change identation of auth schemas

--- src/pages/SignIn/schema.ts
+++ src/pages/SignIn/schema.ts
@@ -1,8 +1,14 @@
 import * as Yup from "yup";
 
 const signInSchema = Yup.object().shape({
-  email: Yup.string().email().required(),
-  password: Yup.string().required().min(5),
+  email: Yup
+    .string()
+    .email()
+    .required(),
+  password: Yup
+    .string()
+    .required()
+    .min(5),
 });
 
 export default signInSchema;
--- src/pages/SignUp/schema.ts
+++ src/pages/SignUp/schema.ts
@@ -1,9 +1,17 @@
 import * as Yup from "yup";
 
 const signUpSchema = Yup.object().shape({
-  name: Yup.string().required(),
-  email: Yup.string().email().required(),
-  password: Yup.string().required().min(5),
+  name: Yup
+    .string()
+    .required(),
+  email: Yup
+    .string()
+    .email()
+    .required(),
+  password: Yup
+    .string()
+    .required()
+    .min(5),
 });
 
 export default signUpSchema;
