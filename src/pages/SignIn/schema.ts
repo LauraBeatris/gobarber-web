06e3ba36a7d2f8e11ec53c9dc1a3d8e6c93cf860
Laura <laurabeatriserafim@gmail.com>
2020-06-14 16:00:45 -0300

    refactor: apply labels to auth validation messages

--- src/pages/SignIn/schema.ts
+++ src/pages/SignIn/schema.ts
@@ -1,14 +1,18 @@
 import * as Yup from "yup";
 
+import i18n from "../../translations/i18n";
+
 const signInSchema = Yup.object().shape({
   email: Yup
     .string()
     .email()
-    .required(),
+    .required()
+    .label(i18n.t("account_form.email")),
   password: Yup
     .string()
     .required()
-    .min(5),
+    .min(5)
+    .label(i18n.t("account_form.password")),
 });
 
 export default signInSchema;
--- src/pages/SignUp/schema.ts
+++ src/pages/SignUp/schema.ts
@@ -1,17 +1,22 @@
 import * as Yup from "yup";
 
+import i18n from "../../translations/i18n";
+
 const signUpSchema = Yup.object().shape({
   name: Yup
     .string()
-    .required(),
+    .required()
+    .label(i18n.t("account_form.full_name")),
   email: Yup
     .string()
     .email()
-    .required(),
+    .required()
+    .label(i18n.t("account_form.email")),
   password: Yup
     .string()
     .required()
-    .min(5),
+    .min(5)
+    .label(i18n.t("account_form.password")),
 });
 
 export default signUpSchema;
