fa09ca24eb9ad2751cfd2a53574968267089fb37
Laura <laurabeatriserafim@gmail.com>
2020-06-14 20:55:51 -0300

    chore: update background color in storybook

--- src/stories/styles.ts
+++ src/stories/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background: ${({ theme }) => theme.colors.white};
+
   display: flex;
   justify-content: center;
   align-items: center;
