2bf47cc1624ac93742b49eb0185c6dc7fdd5bf71
Laura <laurabeatriserafim@gmail.com>
2020-10-01 11:03:28 -0300

    fix: add header above children on app layout

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import Header from "components/Header";
 
 const AppLayout: React.FC = ({ children }) => (
-  <Header>
+  <>
+    <Header />
+
     {children}
-  </Header>
+  </>
 );
 
 export default AppLayout;
