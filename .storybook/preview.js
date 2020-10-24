ce1c708b9acfdb8886c43c74809af9d528cb06b1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-10-24 11:31:34 -0300

    fix: add modal container to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+
 import StylesProvider from '../src/contexts/styles';
+import ModalContainer from '../src/contexts/modal/ModalContainer';
 
 addDecorator(storyFn => (
   <StylesProvider>
+    <ModalContainer>
     {storyFn()}
+    </ModalContainer>
   </StylesProvider>
 ));
